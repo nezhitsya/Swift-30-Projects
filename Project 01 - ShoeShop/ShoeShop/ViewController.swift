7c6e56484d67f692abc4706e648241dc9f1fb410
nezhitsya <huyn9295@naver.com>
2021-03-13 17:47:02 +0900

    first commit

---
+++ Project 01 - ShoeShop/ShoeShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoeShop
+//
+//  Created by 이다영 on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
