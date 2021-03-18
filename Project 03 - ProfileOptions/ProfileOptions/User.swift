976f7fa532dc11f0030ec69aa7b259b384dd7812
nezhitsya <huyn9295@naver.com>
2021-03-18 11:14:59 +0900

    set up model

---
+++ Project 03 - ProfileOptions/ProfileOptions/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  ProfileOptions
+//
+//  Created by 이다영 on 2021/03/18.
+//
+
+import UIKit
+
+class User {
+    var name: String
+    var nickname: String
+    var description: String
+    
+    init(name: String, nickname: "랭리자베스", description: String) {
+        self.name = name
+        self.nickname = nickname
+        self.description = description
+    }
+}
