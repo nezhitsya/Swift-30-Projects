75d9eed7f16cc335b1d6e5c7f27b9e40661d7f62
nezhitsya <huyn9295@naver.com>
2021-03-25 10:44:00 +0900

    first commit

---
+++ Project 05 - ArtGallery/ArtGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtGallery
+//
+//  Created by 이다영 on 2021/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
