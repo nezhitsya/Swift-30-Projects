8025c977226f259115d6db4a1a94bad0abc397d3
nezhitsya <huyn9295@naver.com>
2021-03-27 01:07:41 +0900

    set up table view

--- Project 05 - ArtGallery/ArtGallery/ArtistDetailViewController.swift
+++ Project 05 - ArtGallery/ArtGallery/ArtistDetailViewController.swift
@@ -9,15 +9,24 @@ import UIKit
 
 class ArtistDetailViewController: UIViewController {
     
+    @IBOutlet weak var artTable: UITableView!
+    
     let moreInfoText = "More Info > "
     var selectedArtist: Artist!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = selectedArtist.name
+        
+        artTable.rowHeight = UITableView.automaticDimension
+        artTable.estimatedRowHeight = 300
+        
+        NotificationCenter.default.addObserver(forName: UIContentSizeCategory.didChangeNotification, object: .none, queue: OperationQueue.main) { [weak self] _ in
+            
+            self?.artTable.reloadData()
+        }
     }
-    
 
     /*
     // MARK: - Navigation
@@ -29,4 +38,17 @@ class ArtistDetailViewController: UIViewController {
     }
     */
 
+}
+
+extension ArtistDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedArtist.works.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = artTable.dequeueReusableCell(withIdentifier: "ArtCell", for: indexPath) as! ArtTableViewCell
+        let work = selectedArtist.works[indexPath.row]
+        
+        return cell
+    }
 }
