437a4ebd4d9c45c664971db2f738d3c3c106d875
nezhitsya <huyn9295@naver.com>
2021-03-26 10:29:53 +0900

    set up model

--- Project 05 - ArtGallery/ArtGallery/Model/Artist.swift
+++ Project 05 - ArtGallery/ArtGallery/Model/Artist.swift
@@ -19,4 +19,46 @@ struct Artist {
         self.image = image
         self.works = works
     }
+    
+    static func artistsFromBundle() -> [Artist] {
+        var artists = [Artist]()
+        
+        guard let url = Bundle.main.url(forResource: "artists", withExtension: "json") else {
+            return artists
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            guard let rootObject = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+                return artists
+            }
+            
+            guard  let artistObjects = rootObject["artists"] as? [[String: AnyObject]] else {
+                return artists
+            }
+            
+            for artistObject in artistObjects {
+                if let name = artistObject["name"] as? String,
+                   let bio = artistObject["bio"] as? String,
+                   let imageName = artistObject["image"] as? String,
+                   let image = UIImage(named: imageName),
+                   let worksObject = artistObject["works"] as? [[String: String]] {
+                    var works = [Work]()
+                    for workObject in worksObject {
+                        if let workTitle = workObject["title"],
+                           let workImageName = workObject["image"],
+                           let workImage = UIImage(named: workImageName + ".jpg"),
+                           let info = workObject["info"] {
+                            works.append(Work(title: workTitle, image: workImage, info: info, isExpanded: false))
+                        }
+                    }
+                    let artist = Artist(name: name, bio: bio, image: image, works: works)
+                    artists.append(artist)
+                }
+            }
+        } catch {
+            return artists
+        }
+        return artists
+    }
 }
