7394f5b48b3197cf481a574230f1ca3b34908205
nezhitsya <huyn9295@naver.com>
2021-03-29 12:59:51 +0900

    first commit

---
+++ Project 06 - SnackSearch/SnackSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnackSearch
+//
+//  Created by 이다영 on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
