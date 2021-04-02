94c5c3a1b7ec4516697690e22a1092e65222b7c1
nezhitsya <huyn9295@naver.com>
2021-04-03 01:38:25 +0900

    set up table view

---
+++ Project 08 - Newspaper/Newspaper/NewsTableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  NewsTableViewController.swift
+//  Newspaper
+//
+//  Created by 이다영 on 2021/04/03.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    private let feedParser = RSSParser()
+    private let feedURL = "http://www.apple.com/main/rss/hotnews/hotnews.rss"
+    private var rssItems: [(title: String, description: String, pubDate: String)]
+    private var cellStates: [CellState]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.estimatedRowHeight = 140
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
+        
+        feedParser.parseFeed(feedURL: feedURL) { [weak self] rssItems in
+            
+            self?.rssItems = rssItems
+            self?.cellStates = Array(repeating: .collapsed, count: rssItems.count)
+            
+            DispatchQueue.main.async {
+                self?.tableView.reloadSections(IndexSet(integer: 0), with: .none)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        guard let rssItems = rssItems else {
+            return 0
+        }
+        return rssItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        tableView.beginUpdates()
+        
+        tableView.endUpdates()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
