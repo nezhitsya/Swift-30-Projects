45b6c1139196135b466797c2af5e40abdfe539be
nezhitsya <huyn9295@naver.com>
2021-04-07 02:37:41 +0900

    set up collection view

--- Project 10 - Gallery/Gallery/ViewController.swift
+++ Project 10 - Gallery/Gallery/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var collections = Collections.createCollections()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+    }
+}
