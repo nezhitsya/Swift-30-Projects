019e926aaa01be7cb7c4f59552a990ee8dabb1b3
nezhitsya <huyn9295@naver.com>
2021-04-11 14:33:30 +0900

    set up actions

---
+++ Project 12 - ActiveMenu/ActiveMenu/TransitionManager.swift
@@ -0,0 +1,66 @@
+//
+//  TransitionManager.swift
+//  ActiveMenu
+//
+//  Created by 이다영 on 2021/04/11.
+//
+
+import UIKit
+
+class TransitionManager: NSObject {
+    private var presenting = false
+}
+
+extension TransitionManager: UIViewControllerAnimatedTransitioning {
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let container = transitionContext.containerView
+        let screens: (from: UIViewController, to: UIViewController) = (transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!, transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!)
+        let menuViewController = !self.presenting ? screens.from as! MenuViewController : screens.to as! MenuViewController
+        let bottomViewController = !self.presenting ? screens.to as UIViewController : screens.from as UIViewController
+        let menuView = menuViewController.view
+        let bottomView = bottomViewController.view
+        
+        if (self.presenting) {
+            
+        }
+        
+        container.addSubview(bottomView!)
+        container.addSubview(menuView!)
+    }
+}
+
+func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0.5
+}
+
+func offStage(_ amount: CGFloat) -> CGAffineTransform {
+    return CGAffineTransform(translationX: amount, y: 0)
+}
+
+func offStageMenuController(_ menuViewController: MenuViewController) {
+    menuViewController.view.alpha = 0
+    
+    let topRowOffset: CGFloat = 300
+    let middleRowOffset: CGFloat = 150
+    let bottomRowOffset: CGFloat = 50
+    
+    
+}
+
+func onStageMenuControoler(_ menuViewController: MenuViewController) {
+    menuViewController.view.alpha = 1
+    
+    
+}
+
+extension TransitionManager: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.presenting = true
+        return self
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.presenting = false
+        return self
+    }
+}
