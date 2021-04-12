341820305123edc81b76b5f72ae4ecdd2a3f969a
nezhitsya <huyn9295@naver.com>
2021-04-12 14:21:54 +0900

    set up actions

--- Project 13 - ActiveLogo/ActiveLogo/ViewController.swift
+++ Project 13 - ActiveLogo/ActiveLogo/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CAAnimationDelegate {
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let mask = CALayer()
+        mask.contents = UIImage(named: "front")?.cgImage
+        mask.bounds = CGRect(x: 0, y: 0, width: 100, height: 80)
+        mask.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        mask.position = CGPoint(x: image.frame.size.width/2, y: image.frame.size.height/2)
+        image.layer.mask = mask
+        
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 1
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut), CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)]
+        
+        let initialBounds = NSValue(cgRect: mask.bounds)
+        let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 80, height: 60))
+        let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 2000, height: 2000))
+        
+        keyFrameAnimation.values = [initialBounds, secondBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        mask.add(keyFrameAnimation, forKey: "bounds")
     }
-
-
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        image?.layer.mask = nil
+    }
 }
-
