1d18cfeaeb06c957c0188eb2eae9d57f4c4aab8e
nezhitsya <huyn9295@naver.com>
2021-04-14 03:43:10 +0900

    set up view

--- Project 15 - SlideMenu/SlideMenu/ViewController.swift
+++ Project 15 - SlideMenu/SlideMenu/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
     
     enum vcName: String {
         case first = "FirstViewController"
@@ -20,19 +21,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
-        scrollView.contentOffset.x = view.frame.width
-        
         let firstVC = UIViewController(nibName: vcName.first.rawValue, bundle: nil)
         let secondVC = UIViewController(nibName: vcName.second.rawValue, bundle: nil)
         let thirdVC = UIViewController(nibName: vcName.third.rawValue, bundle: nil)
         add(childViewController: firstVC, toParentViewController: self)
         add(childViewController: secondVC, toParentViewController: self)
         add(childViewController: thirdVC, toParentViewController: self)
+        
+        changeX(ofView: self.imageView, xPosition: view.frame.width)
+        scrollView.addSubview(self.imageView)
+        
+        changeX(ofView: firstVC.view, xPosition: view.frame.width * 1)
+        changeX(ofView: secondVC.view, xPosition: view.frame.width * 2)
+        changeX(ofView: thirdVC.view, xPosition: view.frame.width * 3)
+        
+        scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
+        scrollView.contentOffset.x = view.frame.width
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    private func changeX(ofView view: UIView, xPosition: CGFloat) {
+        var frame = view.frame
+        
+        frame.origin.x = xPosition
+        view.frame = frame
     }
     
     private func add(childViewController: UIViewController, toParentViewController parentViewController: UIViewController) {
