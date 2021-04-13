ea6370bfe82a7f1f28befe9c3b12f4cc107f3997
nezhitsya <huyn9295@naver.com>
2021-04-14 03:12:27 +0900

    set up view

---
+++ Project 15 - SlideMenu/SlideMenu/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  SlideMenu
+//
+//  Created by 이다영 on 2021/04/14.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project 15 - SlideMenu/SlideMenu/ViewController.swift
+++ Project 15 - SlideMenu/SlideMenu/ViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    enum vcName: String {
+        case first = "FirstViewController"
+        case second = "SecondViewController"
+        case third = "ThirdViewController"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
         scrollView.contentOffset.x = view.frame.width
+        
+        let firstVC = UIViewController(nibName: vcName.first.rawValue, bundle: nil)
+        let secondVC = UIViewController(nibName: vcName.second.rawValue, bundle: nil)
+        let thirdVC = UIViewController(nibName: vcName.third.rawValue, bundle: nil)
+        add(childViewController: firstVC, toParentViewController: self)
+        add(childViewController: secondVC, toParentViewController: self)
+        add(childViewController: thirdVC, toParentViewController: self)
     }
 
     override var prefersStatusBarHidden: Bool {
