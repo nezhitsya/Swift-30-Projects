df893f6196486384ddb2d990d041e355562894e1
nezhitsya <huyn9295@naver.com>
2021-04-14 02:40:40 +0900

    set up scrollview

--- Project 15 - SlideMenu/SlideMenu/ViewController.swift
+++ Project 15 - SlideMenu/SlideMenu/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
+        scrollView.contentOffset.x = view.frame.width
     }
 
-
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    private func add(childViewController: UIViewController, toParentViewController parentViewController: UIViewController) {
+        addChild(childViewController)
+        scrollView.addSubview(childViewController.view)
+        childViewController.didMove(toParent: parentViewController)
+    }
 }
 
