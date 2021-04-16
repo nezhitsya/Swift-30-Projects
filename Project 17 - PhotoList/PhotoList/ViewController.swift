8b10bf9292d7e4afc74d481a0247dcf657d7f0a9
nezhitsya <huyn9295@naver.com>
2021-04-17 04:06:30 +0900

    set up tableview

--- Project 17 - PhotoList/PhotoList/ViewController.swift
+++ Project 17 - PhotoList/PhotoList/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+let imageURL = URL(string: "")
+
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Photos"
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
+        return cell
+    }
+    
+    override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        
+    }
+    
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            
+        }
+    }
+    
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        <#code#>
+    }
 }
 
