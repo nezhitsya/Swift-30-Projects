e573bacd0ca2b364cc8c065ed9755668fa63128b
nezhitsya <huyn9295@naver.com>
2021-04-23 02:23:16 +0900

    set up notification

---
+++ Project 18 - Music/Music/AlbumView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumView.swift
+//  Music
+//
+//  Created by 이다영 on 2021/04/23.
+//
+
+import UIKit
+
+class AlbumView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Project 18 - Music/Music/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Music
+//
+//  Created by 이다영 on 2021/04/23.
+//
+
+import Foundation
