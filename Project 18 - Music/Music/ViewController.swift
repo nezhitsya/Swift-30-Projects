046938cce479ad522a09eb912df510073d76e90f
nezhitsya <huyn9295@naver.com>
2021-04-23 02:52:17 +0900

    set up ui

--- Project 18 - Music/Music/ViewController.swift
+++ Project 18 - Music/Music/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var dataTable: UITableView!
+    @IBOutlet var toolbar: UIToolbar!
+    
     private var allAlbums = [Album]()
     private var currentAlbumData: [AlbumData]?
     private var currentAlbumIndex = 0
@@ -17,7 +20,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        func setUI() {
+            navigationController?.navigationBar.isTranslucent = false
+        }
+        
+        func setData() {
+            currentAlbumIndex = 0
+            allAlbums = LibraryAPI.sharedInstance.getAlbum()
+        }
+        
+        func setComponents() {
+            dataTable.backgroundView = nil
+            
+        }
+        
+        setUI()
+        setData()
+        setComponents()
+        showDataForAlbum(at: currentAlbumIndex)
+        
         NotificationCenter.default.addObserver(self, selector: Selector.saveCurrentState, name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+    
+    func showDataForAlbum(at index: Int) {
+        if index < allAlbums.count && index > -1 {
+            let album = allAlbums[index]
+            currentAlbumData = album.tableRepresentation
+        } else {
+            currentAlbumData = nil
+        }
+        dataTable.reloadData()
     }
 
     @objc func saveCurrentState() {
