038d8b8d025786981057ab0f857a1793ada0e23f
nezhitsya <huyn9295@naver.com>
2021-04-25 20:19:19 +0900

    set up first commit

---
+++ Project 19 - PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by 이다영 on 2021/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
