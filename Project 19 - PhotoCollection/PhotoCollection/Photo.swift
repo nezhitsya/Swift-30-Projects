c43a492e90c85f65edbbb998368908dfd8cfa73d
nezhitsya <huyn9295@naver.com>
2021-04-27 09:52:12 +0900

    set up model

---
+++ Project 19 - PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,48 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by 이다영 on 2021/04/27.
+//
+
+import UIKit
+
+class Photo {
+    class func allPhotos() -> [Photo] {
+        var photos = [Photo]()
+        if let URL = Bundle.main.url(forResource: "Photos", withExtension: "plist") {
+            if let photosFromPlist = NSArray(contentsOf: URL) {
+                for dictionary in photosFromPlist {
+                    let photo = Photo(dictionary: dictionary as! NSDictionary)
+                    photos.append(photo)
+                }
+            }
+        }
+        return photos
+    }
+    
+    var caption: String
+    var comment: String
+    var image: UIImage
+    
+    init(caption: String, comment: String, image: UIImage) {
+        self.caption = caption
+        self.comment = comment
+        self.image = image
+    }
+    
+    convenience init(dictionary: NSDictionary) {
+        let caption = dictionary["Caption"] as? String
+        let comment = dictionary["Comment"] as? String
+        let photo = dictionary["Photo"] as? String
+        let image = UIImage(named: photo!)
+        
+        self.init(caption: caption!, comment: comment!, image: image!)
+    }
+    
+    func heightForComment(_ font: UIFont, width: CGFloat) -> CGFloat {
+        let rect = NSString(string: comment).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        
+        return ceil(rect.height)
+    }
+}
