450e4e1e10563bdee75e02c0ae5de1abf1d11a5c
nezhitsya <huyn9295@naver.com>
2021-04-28 10:22:54 +0900

    set up collection view

--- Project 19 - PhotoCollection/PhotoCollection/CollectionViewCell.swift
+++ Project 19 - PhotoCollection/PhotoCollection/CollectionViewCell.swift
@@ -8,5 +8,26 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var imageViewHeightLayoutConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var caption: UILabel!
+    @IBOutlet private weak var comment: UILabel!
     
+    var photo: Photo? {
+        didSet {
+            if let photo = photo {
+                imageView.image = photo.image
+                caption.text = photo.caption
+                comment.text = photo.comment
+            }
+        }
+    }
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        
+        if let attributes = layoutAttributes as? CollectionLayoutAttributes {
+            imageViewHeightLayoutConstraint.constant = attributes.photoHeight
+        }
+    }
 }
--- Project 19 - PhotoCollection/PhotoCollection/ViewController.swift
+++ Project 19 - PhotoCollection/PhotoCollection/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UICollectionViewController {
     
@@ -32,16 +33,29 @@ extension ViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
         
+        cell.photo = photos[(indexPath as NSIndexPath).item]
+        
         return cell
     }
 }
 
 extension ViewController: CollectionLayoutDelegate {
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath, withWidth width: CGFloat) -> CGFloat {
-        <#code#>
+        let photo = photos[(indexPath as NSIndexPath).item]
+        let boundingRect = CGRect(x: 0, y: 0, width: width, height: CGFloat(MAXFLOAT))
+        let rect = AVMakeRect(aspectRatio: photo.image.size, insideRect: boundingRect)
+        
+        return rect.size.height
     }
     
     func collectionView(_ collectionView: UICollectionView, heightForAnnotationAtIndexPath indexPath: IndexPath, withWidth width: CGFloat) -> CGFloat {
-        <#code#>
+        let annotationPadding = CGFloat(4)
+        let annotationHeaderHeight = CGFloat(17)
+        let photo = photos[(indexPath as NSIndexPath).item]
+        let font = UIFont(name: "ArialHebrew", size: 10)!
+        let commentHeight = photo.heightForComment(font, width: width)
+        let height = annotationPadding + annotationHeaderHeight + commentHeight + annotationPadding
+        
+        return height
     }
 }
