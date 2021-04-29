ef4bc986a24438a90808f5b4daec2b1908f49e91
nezhitsya <huyn9295@naver.com>
2021-04-30 02:13:36 +0900

    set up webview

--- Project 21 - Browser/Browser/ViewController.swift
+++ Project 21 - Browser/Browser/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var urlStr: String = "http://www.apple.com"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView.navigationDelegate = self
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+        webView.load(urlStr)
     }
-
-
 }
 
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let str = textField.text {
+            urlStr = "https://" + str
+            webView.load(urlStr)
+        }
+        return false
+    }
+}
+
+extension WKWebView {
+    func load(_ urlString: String) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            load(request)
+        }
+    }
+}
