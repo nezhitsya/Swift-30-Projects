e2e0e762fb978c787dbe5cd4ce3402451061079f
nezhitsya <huyn9295@naver.com>
2021-05-06 12:12:49 +0900

    set up table view

---
+++ Project 23 - Calendar/Calendar/CalendarTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CalendarTableViewCell.swift
+//  Calendar
+//
+//  Created by 이다영 on 2021/05/06.
+//
+
+import UIKit
+
+class CalendarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Project 23 - Calendar/Calendar/ViewController.swift
+++ Project 23 - Calendar/Calendar/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var contactsTable: UITableView!
+    
+    var contacts = [CNContact]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureTableView()
     }
 
-
+    func configureTableView() {
+        contactsTable.delegate = self
+        contactsTable.dataSource = self
+        contactsTable.register(UINib(nibName: "ContactCalendarCell", bundle: nil), forCellReuseIdentifier: "Cell")
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CalendarTableViewCell
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            contacts.remove(at: indexPath.row)
+            contactsTable.reloadData()
+        }
+    }
+}
