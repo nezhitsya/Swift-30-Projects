ca656ca67e1e2bfde1ad7563b7f6a07fc8b6267f
nezhitsya <huyn9295@naver.com>
2021-05-08 12:41:32 +0900

    set up controller

--- Project 23 - Calendar/Calendar/AddContactViewController.swift
+++ Project 23 - Calendar/Calendar/AddContactViewController.swift
@@ -20,6 +20,7 @@ class AddContactViewController: UIViewController {
     
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     var currentlySelectedMonthIndex = 1
+    var delegate: AddContactViewControllerDelegate!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,13 +42,40 @@ class AddContactViewController: UIViewController {
 }
 
 extension AddContactViewController: CNContactPickerDelegate {
+    @IBAction func showContacts(_ sender: AnyObject) {
+        let contactPickerViewController = CNContactPickerViewController()
+        
+        contactPickerViewController.predicateForEnablingContact = NSPredicate(format: "Birthday != nil")
+        contactPickerViewController.delegate = self
+        contactPickerViewController.displayedPropertyKeys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactEmailAddressesKey, CNContactBirthdayKey, CNContactImageDataKey]
+        
+        present(contactPickerViewController, animated: true, completion: nil)
+    }
     
+    func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
+        delegate.didFetchContacts([contact])
+        navigationController?.popViewController(animated: true)
+    }
 }
 
 extension AddContactViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return months.count
+    }
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return months[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentlySelectedMonthIndex = row + 1
+    }
 }
 
 extension AddContactViewController: UITextFieldDelegate {
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        
+        return true
+    }
 }
