0bc0f290ffe18140aee1cfdb87f5120abda58a74
nezhitsya <huyn9295@naver.com>
2021-05-11 11:15:33 +0900

    set up table view

--- Project 24 - Memo/Memo/ViewController.swift
+++ Project 24 - Memo/Memo/ViewController.swift
@@ -10,19 +10,39 @@ import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
     
+    var memos: [NSManagedObject] = []
+    
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        self.fetchCoreData(managedContext)
+    }
+    
+    func fetchCoreData(_ managedContext: NSManagedObjectContext) {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Memo")
+        
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            memos = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return memos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let memo = memos[indexPath.row]
+        
+        cell.textLabel!.text = memo.value(forKey: "name") as? String
         
         return cell
     }
