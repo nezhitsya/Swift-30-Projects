5ec0f7821f7fcb79803c315ef7c7b812f84a9ce1
nezhitsya <huyn9295@naver.com>
2021-05-11 11:01:50 +0900

    set up table view

--- Project 24 - Memo/Memo/ViewController.swift
+++ Project 24 - Memo/Memo/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
 }
 
