7d3029fdef5c40d156120593fc244cccebfe36c2
nezhitsya <huyn9295@naver.com>
2021-05-13 08:54:44 +0900

    set up app delegae

--- Project 26 - 3DTouch/3DTouch/AppDelegate.swift
+++ Project 26 - 3DTouch/3DTouch/AppDelegate.swift
@@ -8,14 +8,45 @@
 import UIKit
 import CoreData
 
+enum Shortcut: String {
+    case openBlue = "Open"
+}
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        var isLaunchedFromQuickAction = false
+        
+        if let shortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            isLaunchedFromQuickAction = true
+            let _ = handleQuickAction(shortcutItem)
+        } else {
+            self.window?.backgroundColor = UIColor.white
+        }
+        
+        return !isLaunchedFromQuickAction
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
+    }
+    
+    func handleQuickAction(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var quickActionHandled = false
+        let type = shortcutItem.type.components(separatedBy: ".").last!
+        
+        if let shortcutType = Shortcut.init(rawValue: type) {
+            switch shortcutType {
+            case .openBlue:
+                self.window?.backgroundColor = UIColor(red: 153.0/255.0, green: 153.0/255.0, blue: 204.0/255.0, alpha: 1.0)
+                quickActionHandled = true
+            }
+        }
+        return quickActionHandled
     }
 
     // MARK: UISceneSession Lifecycle
