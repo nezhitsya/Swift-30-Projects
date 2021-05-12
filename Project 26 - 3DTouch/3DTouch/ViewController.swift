b7ca3dfae493f59e69dd5e1b4a19b6f0d294a2a0
nezhitsya <huyn9295@naver.com>
2021-05-13 08:47:30 +0900

    set up controller

--- Project 26 - 3DTouch/3DTouch/ViewController.swift
+++ Project 26 - 3DTouch/3DTouch/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var forceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        if traitCollection.forceTouchCapability == UIForceTouchCapability.available {
+            if touch.force >= touch.maximumPossibleForce {
+                forceLabel.text = "385+ g"
+            } else {
+                let force = touch.force / touch.maximumPossibleForce
+                let grams = Int(force * 385)
+                
+                forceLabel.text = "\(grams) g"
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        forceLabel.text = "0 g"
+    }
 }
 
