8861e10b82780007d3b29d361f8941c0546d8683
nezhitsya <huyn9295@naver.com>
2021-05-14 10:47:53 +0900

    set up app delegate

--- Project 27 - Notification/Notification/AppDelegate.swift
+++ Project 27 - Notification/Notification/AppDelegate.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { accepted, error in
+            if !accepted {
+                print("Notification access denied.")
+            }
+        }
+        
+        let action = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: [])
+        let category = UNNotificationCategory(identifier: "normal", actions: [action], intentIdentifiers: [], options: [])
+        
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+        
         return true
     }
 
@@ -76,6 +88,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+    
+    // MARK: - Notification
+    func scheduleNotification(at date: Date) {
+        UNUserNotificationCenter.current().delegate = self
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents(in: .current, from: date)
+        let newComponents = DateComponents.init(calendar: calendar, timeZone: .current, month: components.month, day: components.day, hour: components.hour, minute: components.minute)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+        let content = UNMutableNotificationContent()
 
+        content.title = "Reminder"
+        content.body = "Wake up! You have something to do!"
+        content.sound = UNNotificationSound.default
+        content.categoryIdentifier = "normal"
+        
+        if let path = Bundle.main.path(forResource: "clock", ofType: "jpg") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                let attachment = try UNNotificationAttachment(identifier: "clock", url: url, options: nil)
+                content.attachments = [attachment]
+            } catch {
+                print("Image not loaded.")
+            }
+        }
+        
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Error: " + error.localizedDescription)
+            }
+        }
+    }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.actionIdentifier == "remind" {
+            let newDate = Date(timeInterval: 60, since: Date())
+            scheduleNotification(at: newDate)
+        }
+    }
+}
