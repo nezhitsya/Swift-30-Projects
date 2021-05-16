7cd2597b8e7e0c10bcd59115c36216eb023f8f8f
nezhitsya <huyn9295@naver.com>
2021-05-16 11:27:56 +0900

    first commit

---
+++ Project 29 - SNS/SNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNS
+//
+//  Created by 이다영 on 2021/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
