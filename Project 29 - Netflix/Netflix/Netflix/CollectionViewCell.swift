033adb56a2b546332969acd58ebed6ab420c85a7
nezhitsya <huyn9295@naver.com>
2021-06-24 11:30:34 +0900

    first commit

---
+++ Project 29 - Netflix/Netflix/Netflix/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Netflix
+//
+//  Created by 이다영 on 2021/06/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+}
---
+++ Project 29 - Netflix/Netflix/Netflix/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by 이다영 on 2021/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainPoster: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        
+        mainPoster.addBlackGradientLayerInForeground(frame: mainPoster.frame, colors: [.clear, .black])
+    }
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+}
+
+extension UIView {
+    
+    func addBlackGradientLayerInForeground(frame: CGRect, colors: [UIColor]) {
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.colors = colors.map{$0.cgColor}
+        gradient.locations = [0.0, 1.0]
+        self.layer.addSublayer(gradient)
+    }
+    
+}
