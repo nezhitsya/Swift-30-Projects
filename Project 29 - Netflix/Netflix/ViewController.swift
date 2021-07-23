8e66f250f97806b375677f4427a8fa99455c58ad
nezhitsya <huyn9295@naver.com>
2021-07-23 14:38:20 +0900

    set up parse json

--- Project 29 - Netflix/Netflix/ViewController.swift
+++ Project 29 - Netflix/Netflix/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var scifiCollectionView: UICollectionView!
     
     private var contents = [Contents]()
+    var contentsList: Contents!
+    var dramaList: [String] = []
+    var scifiList: [String] = []
+    var horrorList: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,20 +28,52 @@ class ViewController: UIViewController {
         mainPoster.addBlackGradientLayerInForeground(frame: mainPoster.frame, colors: [.clear, .black])
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        parseJSON()
+    }
+    
+    func parseJSON() {
+        guard let url = URL(string: "https://raw.githubusercontent.com/nezhitsya/Swift-30-Projects/master/Project%2029%20-%20Netflix/contents.json") else {
+            print("api is down")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if error == nil && data != nil {
+                do {
+                    if let response = try? JSONDecoder().decode(Contents.self, from: data!) {
+                        DispatchQueue.main.async {
+                            self.contentsList = response
+                            if self.contentsList.genre == "drama" {
+                                self.dramaList.append(self.contentsList.title)
+                            } else if self.contentsList.genre == "horror" {
+                                self.horrorList.append(self.contentsList.title)
+                            } else {
+                                self.scifiList.append(self.contentsList.title)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return dramaList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        let dramaCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
         
-        cell.posterImage.image = UIImage(named: "poster")
+        dramaCell.posterImage.image = UIImage(named: "poster")
         
-        return cell
+        return dramaCell
     }
     
 }
