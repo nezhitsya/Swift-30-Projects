c595fa3234104ece7321d0478f6a9bde79ea9ad3
nezhitsya <huyn9295@naver.com>
2021-10-03 22:31:43 +0900

    set up cell

--- Project 30 - WeatherApp/WeatherApp/View/CurrentWeather/DaysTableViewCell.swift
+++ Project 30 - WeatherApp/WeatherApp/View/CurrentWeather/DaysTableViewCell.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 class DaysTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var tempMaxLabel: UILabel!
+    @IBOutlet weak var tempMinLabel: UILabel!
+    
+    var weatherDayList: List? {
+        didSet {
+            guard let dayTime = weatherDayList?.dtTxt,
+                  let iconName = weatherDayList?.weather.first?.icon else {
+                      return
+                  }
+            dateLabel.text = getDayString(getData: dayTime)
+            weatherIconImageView.image = UIImage(named: iconName)
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,6 +34,33 @@ class DaysTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func getDayString(getData: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        guard let formatDate = dateFormatter.date(from: getData),
+              let dayCnt = formatDate.dayNumberOfWeek(),
+              let day = Week(rawValue: dayCnt) else {
+                  return " "
+              }
+        return day.StringValue
+    }
+    
+    func config(weather data: List, fahrenheitOrCelsius: FahrenheitOrCelsius) {
+        weatherDayList = data
+        
+        let emoji = fahrenheitOrCelsius.emoji
+        
+        switch fahrenheitOrCelsius {
+        case .Celsius:
+            tempMaxLabel.text = data.main.tempMin.makeCelsius() + emoji
+            tempMinLabel.text = data.main.tempMin.makeCelsius() + emoji
+        case .Fahrenheit:
+            tempMaxLabel.text = data.main.tempMax.makeFahrenheit() + emoji
+            tempMinLabel.text = data.main.tempMin.makeFahrenheit() + emoji
+        }
     }
     
 }
