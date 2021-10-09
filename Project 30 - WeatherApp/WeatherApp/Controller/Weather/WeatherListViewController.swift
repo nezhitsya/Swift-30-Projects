5f3bf863598a5e7b8ed2f39317b2b037ed6e5619
nezhitsya <huyn9295@naver.com>
2021-10-09 22:36:24 +0900

    set up ui

---
+++ Project 30 - WeatherApp/WeatherApp/Controller/Weather/WeatherListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherListViewController.swift
+//  WeatherApp
+//
+//  Created by 이다영 on 2021/10/09.
+//
+
+import UIKit
+import WebKit
+
+class WeatherListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
